94da4b8b29d1b7b827ba0e9147e990466676960b
ZhiXiong TANG <dvorak4tzx@gmail.com>
2024-08-17 15:41:30 +0800

    fix

--- src/graph.cpp
+++ src/graph.cpp
@@ -31,48 +31,45 @@ std::map<int, std::vector<std::vector<int64_t>>> Sequences::search_in(const std:
 }
 
 using State = ZigzagPathGenerator::State;
-std::optional<ZigzagPath>
-ZigzagPathGenerator::Path(const State &state, const int64_t source, //
-        const DiGraph *self,                      //
-        const unordered_map<State, State> &pmap,
-        const unordered_map<State, double> &dmap) 
-    {
-        std::vector<State> states;
-        int64_t target = std::get<0>(state);
-        int dir = -std::get<1>(state);
-        double dist = dmap.at(state);
-        auto cursor = state;
-        while (true) {
-            auto prev = pmap.find(cursor);
-            if (prev == pmap.end()) {
-                // assert cursor at source
-                if (std::get<0>(cursor) != source) {
-                    return {};
-                }
-                states.push_back({source, -std::get<1>(cursor)});
-                break;
-            }
-            cursor = prev->second;
-            states.push_back(cursor);
-        }
-        std::reverse(states.begin(), states.end());
-        size_t N = states.size();
-        if (N % 2 != 0) {
-            return {};
-        }
-        auto nodes = std::vector<int64_t>{};
-        auto dirs = std::vector<int>{};
-        for (size_t i = 0; i < N; i += 2) {
-            if (std::get<0>(states[i]) != std::get<0>(states[i + 1])) {
+std::optional<ZigzagPath> ZigzagPathGenerator::Path(const State &state, const int64_t source,  //
+                                                    const DiGraph *self,                       //
+                                                    const unordered_map<State, State> &pmap,
+                                                    const unordered_map<State, double> &dmap) {
+    std::vector<State> states;
+    int64_t target = std::get<0>(state);
+    int dir = -std::get<1>(state);
+    double dist = dmap.at(state);
+    auto cursor = state;
+    while (true) {
+        auto prev = pmap.find(cursor);
+        if (prev == pmap.end()) {
+            // assert cursor at source
+            if (std::get<0>(cursor) != source) {
                 return {};
             }
-            nodes.push_back(std::get<0>(states[i]));
-            dirs.push_back(
-                std::get<1>(states[i]) < std::get<1>(states[i + 1]) ? 1 : -1);
+            states.push_back({source, -std::get<1>(cursor)});
+            break;
         }
-        nodes.push_back(target);
-        dirs.push_back(dir);
-        return ZigzagPath(self, dist, nodes, dirs);
+        cursor = prev->second;
+        states.push_back(cursor);
     }
+    std::reverse(states.begin(), states.end());
+    size_t N = states.size();
+    if (N % 2 != 0) {
+        return {};
+    }
+    auto nodes = std::vector<int64_t>{};
+    auto dirs = std::vector<int>{};
+    for (size_t i = 0; i < N; i += 2) {
+        if (std::get<0>(states[i]) != std::get<0>(states[i + 1])) {
+            return {};
+        }
+        nodes.push_back(std::get<0>(states[i]));
+        dirs.push_back(std::get<1>(states[i]) < std::get<1>(states[i + 1]) ? 1 : -1);
+    }
+    nodes.push_back(target);
+    dirs.push_back(dir);
+    return ZigzagPath(self, dist, nodes, dirs);
+}
 
 }  // namespace cubao
--- src/graph.hpp
+++ src/graph.hpp
@@ -43,21 +43,15 @@ struct Path {
     std::optional<std::tuple<int64_t, Binding>> binding;
 };
 
-struct ZigzagPath : Path
-{
+struct ZigzagPath : Path {
     // two-way routing on a DiGraph
     ZigzagPath() = default;
-    ZigzagPath(const DiGraph *graph, double dist,
-               const std::vector<int64_t> &nodes,
-               const std::vector<int> &directions)
-        : Path(graph, dist, nodes), directions(directions)
-    {
-    }
+    ZigzagPath(const DiGraph *graph, double dist, const std::vector<int64_t> &nodes, const std::vector<int> &directions)
+        : Path(graph, dist, nodes), directions(directions) {}
     std::vector<int> directions;
 };
 
-struct ShortestPathGenerator
-{
+struct ShortestPathGenerator {
     const DiGraph *graph{nullptr};
     unordered_map<int64_t, int64_t> prevs;
     unordered_map<int64_t, double> dists;
@@ -66,19 +60,13 @@ struct ShortestPathGenerator
     double cutoff{0.0};
     std::optional<Click> source;
     std::optional<Click> target;
-    bool ready() const {
-        return graph && cutoff > 0 && ((bool)source ^ (bool)target);
-    }
+    bool ready() const { return graph && cutoff > 0 && ((bool)source ^ (bool)target); }
 };
 
-struct ZigzagPathGenerator
-{
+struct ZigzagPathGenerator {
     using State = std::tuple<int64_t, int>;
     ZigzagPathGenerator() = default;
-    ZigzagPathGenerator(const DiGraph *graph, double cutoff)
-        : graph(graph), cutoff(cutoff)
-    {
-    }
+    ZigzagPathGenerator(const DiGraph *graph, double cutoff) : graph(graph), cutoff(cutoff) {}
 
     const DiGraph *graph{nullptr};
     double cutoff{0.0};
@@ -88,11 +76,10 @@ struct ZigzagPathGenerator
 
     bool ready() const { return graph && cutoff > 0 && source; }
 
-    static std::optional<ZigzagPath>
-    Path(const State &state, const int64_t source, //
-         const DiGraph *self,                      //
-         const unordered_map<State, State> &pmap,
-         const unordered_map<State, double> &dmap);
+    static std::optional<ZigzagPath> Path(const State &state, const int64_t source,  //
+                                          const DiGraph *self,                       //
+                                          const unordered_map<State, State> &pmap,
+                                          const unordered_map<State, double> &dmap);
 };
 
 }  // namespace cubao
