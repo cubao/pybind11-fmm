90b6319f689c9c7d1a67a1d30407f72c97029a45
ZhiXiong TANG <dvorak4tzx@gmail.com>
2024-08-17 11:41:46 +0800

    add Eigen from https://github.com/cubao/eigen-core-amalgamate, lint code

--- src/fmmpy/__init__.py
+++ src/fmmpy/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import annotations
 
 from ._core import add
+
+__all__ = ["add"]
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from fmmpy import add
+
 
 def test_add():
     assert add(1, 2) == 3
